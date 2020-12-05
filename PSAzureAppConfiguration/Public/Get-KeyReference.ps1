0bb6b3e441494d47ae40c25a5f6c9dd89aa2704b
Michiel Thai <Michiel.Thai@gmail.com>
2020-12-05 15:23:37 +0100

    add pester

--- PSAzureAppConfiguration/Public/Get-KeyReference.ps1
+++ PSAzureAppConfiguration/Public/Get-KeyReference.ps1
@@ -1,8 +1,18 @@
 function Get-KeyReference {
+    <#
+        .SYNOPSIS
+            Parses a string and extracts the tokenized strings
+            Default prefix and suffix is '$(' and ')'
+            Only words chars and dots are allowed as key strings
+        .EXAMPLE
+            $string = 'asdfsadf'
+            Get-KeyReference -String 'MyServer=$(MyApp.Database.Server);Database=$(MyApp.Database.Name);Trusted_Connection=Yes'
+            Expected Output: @('MyApp.Database.Server','MyApp.Database.Name')
+    #>
     param(
-        $string = 'Server=$(Lookup.Database.Server);Database=$(Lookup.Database.Name);Trusted_Connection=Yes'
+        [string] $String
         ,
-        $regex = '\$\([\w.]*\)'
+        [string] $regex = '\$\([\w.]*\)'
     )
     $references = ($string | select-string -pattern $regex -AllMatches).Matches.Value
     if ($references) {
--- Tests/Unit.Tests.ps1
+++ Tests/Unit.Tests.ps1
@@ -9,19 +9,50 @@ BeforeAll {
     $global:Module = Import-Module $moduleOutputPath -Force -PassThru -ErrorAction SilentlyContinue
     if (-not $module) {
         Write-Warning "No importable module found, building.."
-        $global:Module = Import-Module $projectRoot\$moduleName -PassThru
+        $modulePath = "$projectRoot\$moduleName\$moduleName.psm1"
+        Write-Host $modulePath
+        $global:Module = Import-Module $modulePath -PassThru -Force
+        $Module
     }
+    $global:ExportedCommands = $Module.ExportedCommands.GetEnumerator() | ForEach-Object { $_.Key }
+    Write-Host $ExportedCommands
 }
+
 Describe 'Build Output' {
     BeforeAll {
         $global:publicFiles = Get-ChildItem -Path "$projectRoot\$moduleName\Public" -File
     }
     it 'should have ExportedCommands' {
-        $Module.ExportedCommands | Should -Not -Be -$null
+        $ExportedCommands | Should -Not -Be -$null
     }
     it 'public functions should match exported commands' {
-        $ExportedCommands = ($Module.ExportedCommands.GetEnumerator() | Measure-Object).Count
         $numberOfPublicFunctionFiles = ($publicFiles | Measure-Object).Count
-        $exportedCommands | Should -Be $numberOfPublicFunctionFiles
+        $ExportedCommands.Count | Should -Be $numberOfPublicFunctionFiles
     }
+}
+Describe 'Comment Based Examples' {
+    BeforeAll {
+        $scriptPath = 'https://raw.githubusercontent.com/mieel/snippets/master/powershell/Get-CommandHelpExample.ps1'
+        $script = (New-Object System.Net.WebClient).DownloadString($scriptPath)
+        Invoke-Expression $script
+    }
+    ForEach ($command in $ExportedCommands) {
+        Write-Verbose $Command
+        $global:example = Get-CommandHelpExample $command
+        if ($null -ne $example.Assertion) {
+            it "$command - assert expected output in help example" {
+                ## Test the code block if Expected Output is specified
+                $expectedOutput = Invoke-Expression ($Example.Assertion | Out-String)
+                $actualOutput = Invoke-Expression $Example.Scriptblock
+
+                $actualOutput | Should -Be $expectedOutput
+
+            }
+        } else {
+            Write-Verbose 'no expected output found'
+        }
+    }
+}
+Describe 'Get-KeyReference' {
+
 }
