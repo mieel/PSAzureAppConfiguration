2552cd879d2d75dc67ec9b9db84db4ee5268bf8f
Michiel Thai <Michiel.Thai@gmail.com>
2020-12-09 11:11:50 +0100

    re

--- PSAzureAppConfiguration/Public/Get-AppConfigurationFeature.ps1
+++ PSAzureAppConfiguration/Public/Get-AppConfigurationFeature.ps1
@@ -1,4 +1,10 @@
 function Get-AppConfigurationFeature {
+    <#
+        .SYNOPSIS
+            Lists features from an Azure App Configuration
+        .EXAMPLE
+            Get-AppConfigurationFeature -Store $Store
+    #>
     [cmdletbinding()]
     param(
         [string] $Feature = '*'
@@ -8,17 +14,13 @@ function Get-AppConfigurationFeature {
         ,
         [string] $Label = '*'
         ,
-        [switch] $NoResolveSecret
-        ,
         [switch] $ExcludeNoLabel
-        ,
-        [switch] $ConvertReferences
     )
-    # az appconfig kv list -h
-    if (-not $NoResolveSecret) {
-        $resolveKv = '--resolve-keyvault'
+    $labelQuery = "\0,$Label"
+    if ( -not $PSBoundParameters.ExcludeNoLabel ) {
+        $labelQuery = "$Label"
     }
-    $features = az appconfig feature list --name $Store --label \0,$Label --feature $Feature $resolveKv | ConvertFrom-Json
+    $features = az appconfig feature list --name $Store --label $labelQuery --feature $Feature  | ConvertFrom-Json
     # when multiple keys are returned, keep the one with the label
     $Output = $features | Group-object -Property Key | ForEach-Object {
         $_.Group | Sort-Object -Property Label -Descending | Select-Object -First 1
